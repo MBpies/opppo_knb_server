dbe0b87e5a5622e52c2cc48b9d5c8c2b39ba64a8
Maxim Buldakov <buldakov.m99@gmail.com>
2021-04-14 18:18:47 +0700

    Вернул склалхимию потому что я дурак, добавил человеческие жсон ответы, обновил табблицу в бд, создал заготовку для следующего модуля программы

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import werkzeug
 from flask import Flask, request
-from datetime import datetime
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,12 +9,20 @@ json = FlaskJSON(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Python_proj\\knb_server\\test.db'  # !!исправьте под себя!!
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = "False"
+lobbyList = []
+availablePlayers = []
+
+
+class Lobby():
+    lobbyId = 0
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)  # игнорируйте эти предупрежждения, они чинятся в рантайме
     username = db.Column(db.String(15), unique=True, nullable=False)
     password = db.Column(db.String(20), unique=False, nullable=False)
+    wins = db.Column(db.Integer, default=0)
+    losses = db.Column(db.Integer, default=0)
 
 
 # обработка по адресам
@@ -24,22 +31,31 @@ def auth():
     content = request.get_json()
     username = content["login"]
     password = content["password"]
-    if (content['type'] == "registration"):
+    if content['type'] == "registration":
         checker = User.query.filter_by(username=username).first()
-        if (checker is not None):  # проверка если логин уже есть
-            return "already exist"
+        if checker is not None:  # проверка если логин уже есть
+            return json_response(status_=409, type="registration", status="login exists")
         db.session.add(User(username=username, password=password))  # закинуть в бд
         db.session.commit()  # зачекинить изменения
-        return "All good"
+        return json_response(status_=200, type="registration", status="ok")
 
-    if (content['type'] == "authorization"):
+    if content['type'] == "authorization":
         checker = User.query.filter_by(username=username).first()
-        if (checker is None):  # проверка существует ли логин
-            return "dosent exist"
-        if (checker.password == password):
-            return "All good"
+        if checker is None:  # проверка существует ли логин
+            return json_response(status_=404, type="authorization", status="login doesn't exists")
+        if checker.password == password:
+            return json_response(status_=200, type="authorization", status="ok")
         else:
-            return "password error"
+            return json_response(status_=401, type="authorization", status="login error")
+
+
+@app.route('/lobby', methods=['POST'])
+def lobby():
+    content = request.get_json()
+    if content['type'] == "createLobby":
+        return " error"
+    if content['type'] == "connectToLobby":
+        return " error"
 
 
 # Обработка ошибок
